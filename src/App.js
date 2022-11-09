521ba8471c873924497bf19c9c0dcdd006105477
JHKIMS <wjdgns5131@gmail.com>
2022-11-09 15:29:17 +0900

    React_Todo_state을 이용한 리스트 제거하기(map, filter)

--- src/App.js
+++ src/App.js
@@ -2,6 +2,42 @@ import React, { Component } from "react";
 import "./App.css";
 
 export default class App extends Component {
+  btnStyle = {
+    color: "#fff",
+    border: "none",
+    padding: "5px 9px",
+    borderRadius: "50%",
+    cursor: "pointer",
+    float: "right",
+  };
+
+  bottomLine = () => {
+    return {
+      padding: "10px",
+      borderBottom: "1px #ccc dotted",
+      textDecoration: "none",
+    };
+  };
+  handleClick = (id) => {
+    let newTodoData = this.state.todoData.filter((data) => data.id !== id);
+    console.log('newTodoData', newTodoData);
+    this.setState({todoData: newTodoData});
+  }
+state={
+  todoData : [
+    {
+      id: "1",
+      title: "공부하기",
+      completed: true,
+    },
+    {
+      id: "2",
+      title: "청소하기",
+      completed: false,
+    },
+  ]
+}
+  
   render() {
     return (
       <div className="container">
@@ -9,6 +45,15 @@ export default class App extends Component {
           <div className="title">
             <h1>할 일 목록</h1>
           </div>
+
+          {this.state.todoData.map((data) => (
+            <div style={this.bottomLine()} key={data.id}>
+              <input type="checkbox" defaultChecked={false} />
+              {/* 할 일 목록 적는 곳 */}
+                {data.title}
+              <button style={this.btnStyle} onClick={() => this.handleClick(data.id)}>X</button>
+            </div>
+          ))}
         </div>
       </div>
     );
